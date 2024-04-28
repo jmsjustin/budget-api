34ccf2d22c7406dc902ebc8aaa9be80da927ae6d
Justin Smith <jmsjustin@gmail.com>
2024-04-28 12:53:44 -0400

    Add update action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -16,4 +16,13 @@ class CategoriesController < ApplicationController
     @category = Category.find_by(id: params[:id])
     render :show
   end
+
+  def update
+    @category = Category.find_by(id: params[:id])
+    @category.update(
+      name: params[:name] || @category.name,
+      recommended_percent: params[:recommended_percent] || @category.recommended_percent,
+    )
+    render :show
+  end
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -23,4 +23,13 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal ["id", "name", "recommended_percent", "created_at", "updated_at"], data.keys
   end
+
+  test "update" do
+    photo = Category.first
+    patch "/categories/#{category.id}.json", params: { name: "Updated name" }
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal "Updated name", data["name"]
+  end
 end
