e99c4fa3407263719b0b5a735dcfe8abec131a83
Justin Smith <jmsjustin@gmail.com>
2024-04-28 12:58:28 -0400

    Add destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   post "/categories" => "categories#create"
   get "/categories/:id" => "categories#show"
   patch "/categories/:id" => "categories#update"
+  delete "/categories/:id" => "categories#destroy"
 end
