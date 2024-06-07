65432585cb42f0c01477c50b175ccfe703f8e04b
Justin Smith <jmsjustin@gmail.com>
2024-06-06 20:14:45 -0400

    configured cors to link the frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "example.com", "localhost:5173", "<budget-frontend-app.onrender.com/>"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
